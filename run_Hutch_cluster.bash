6bd7930c06ca3e68c9cb1a2a89743554eab9b664
Arjun Aditham <aaditham@rhino03.fhcrc.org>
2024-10-29 17:10:26 -0700

    update site_numbering_map.csv and re-run

--- run_Hutch_cluster.bash
+++ run_Hutch_cluster.bash
@@ -1,3 +1,5 @@
 #!/bin/bash
+#
+#SBATCH -c 16
 
-snakemake -j 32 -s dms-vep-pipeline-3/Snakefile --software-deployment-method conda --conda-frontend conda --rerun-incomplete
+snakemake -j 16 -s dms-vep-pipeline-3/Snakefile --software-deployment-method conda --conda-frontend conda --rerun-incomplete
