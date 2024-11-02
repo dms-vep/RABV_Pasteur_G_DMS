c411490f642d83ee6886d62b6730f63133f44fdd
Jesse Bloom <jbloom@fredhutch.org>
2024-11-02 16:01:43 -0700

    add a summaries plot that has antibody and escape data (#23)
    * set up to make overall summaries plot
    
    * re-run pipeline

--- run_Hutch_cluster.bash
+++ run_Hutch_cluster.bash
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-snakemake -j 16 -s dms-vep-pipeline-3/Snakefile --software-deployment-method conda --rerun-incomplete
+snakemake -s dms-vep-pipeline-3/Snakefile --unlock
+
+snakemake -j 8 -s dms-vep-pipeline-3/Snakefile --software-deployment-method conda --rerun-incomplete
